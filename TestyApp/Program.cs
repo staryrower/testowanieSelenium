7fb28c190d85dd733b4b1bdc6061821810411f4e
staryrower <katarzynaaluczynska@gmail.com>
2020-04-20 15:36:41 +0200

    testowanie

---
+++ TestyApp/Program.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestyApp
+{
+    class Program
+    {
+        IWebDriver driver = new FirefoxDriver();
+
+        static void Main(string[] args)
+        {
+          
+
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("http://www.executeautomation.com/demosite/index.html");
+
+            Console.WriteLine("Otwarto URL");
+
+        }
+        [Test]
+        public void ExecuteTest()
+
+        {
+            TestSetMetods.SelectDropDown(driver, "TitleId", "Ms.", "Id");
+
+            TestSetMetods.EnterText(driver, "Initial", "testowanie", "Name");
+
+            TestSetMetods.EnterText(driver, "FirstName", "Katarzyna", "Id");
+
+            TestSetMetods.EnterText(driver, "MiddleName", "Łuczyńska", "Id");
+
+            TestSetMetods.SelectRadioButton(driver, "Female", "Name");
+
+            TestSetMetods.SelectCheckBox(driver, "english", "Name");
+
+            Console.WriteLine("Wartość z pola Tytył to" + TestGetMetods.GetText(driver, "TitleId","Id"));
+
+            Console.WriteLine("Wartość z pola Initial to " + TestGetMetods.GetText(driver, "Initial", "Name"));
+
+            Console.WriteLine("Wartość z pola FirstName to " + TestGetMetods.GetText(driver, "FirstName", "Id"));
+
+            Console.WriteLine("Wartość z pola MiddleName to " + TestGetMetods.GetText(driver, "MiddleName", "Id"));
+
+
+
+            TestSetMetods.Click(driver, "Save", "Name");
+
+            System.Threading.Thread.Sleep(3000);
+        }
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+
+            Console.WriteLine("Zamknięto przeglądarkę");
+
+        }
+
+
+
+    }
+}
---
+++ TestyApp/TestSetMetods.cs
@@ -0,0 +1,76 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestyApp
+{
+    class TestSetMetods
+    {
+        //Wprowadzanie tekstu
+
+        public static void EnterText(IWebDriver driver, string element, string value, string elementtype)
+        {
+
+            if (elementtype == "Id")
+                driver.FindElement(By.Id(element)).SendKeys(value);
+
+            if (elementtype == "Name") 
+                driver.FindElement(By.Name(element)).SendKeys(value);
+        }
+
+
+        //Klikanie 
+
+        public static void Click(IWebDriver driver, string element,  string elementtype) {
+
+            if (elementtype == "Id")
+                driver.FindElement(By.Id(element)).Click();
+
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).Click();
+        }
+
+        //Drop down
+
+        public static void SelectDropDown(IWebDriver driver, string element, string value, string elementtype)
+        {
+
+            if (elementtype == "Id")
+               new SelectElement( driver.FindElement(By.Id(element))).SelectByText(value);
+
+            if (elementtype == "Name")
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+
+       
+        }
+
+        //Radio Button
+
+        public static void SelectRadioButton(IWebDriver driver, string element, string elementtype)
+        {
+            if (elementtype == "Id")
+                driver.FindElement(By.Id(element)).Click();
+
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).Click();
+        }
+
+
+        //CheckBox
+
+        public static void SelectCheckBox(IWebDriver driver, string element, string elementtype)
+        {
+            if (elementtype == "Id")
+                driver.FindElement(By.Id(element)).Click();
+
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).Click();
+        }
+    }
+
+    }
+
